7eb75337b97f529fd64fe33776982f1570c91cde
korrawit <korrawitc@hotmail.com>
2017-09-15 10:34:15 +0700

    add direction

--- models.py
+++ models.py
@@ -1,17 +1,32 @@
+DIR_UP = 1
+DIR_RIGHT = 2
+DIR_DOWN = 3
+DIR_LEFT = 4
+ 
+DIR_OFFSET = { DIR_UP: (0,1),
+               DIR_RIGHT: (1,0),
+               DIR_DOWN: (0,-1),
+               DIR_LEFT:    (-1,0) }
+
 class Snake:
+    BLOCK_SIZE = 16
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
         self.wait_time = 0
+        self.direction = DIR_UP
  
     def update(self, delta):
         self.wait_time += delta
         if self.wait_time < Snake.MOVE_WAIT:
             return
-        if self.x > self.world.width:
+        if self.x > self.world.width or self.x < 0:
             self.x = 0
-        self.x += 16
+        if self.y > self.world.height or self.y < 0:
+            self.y = 0
+        self.x += DIR_OFFSET[self.direction][0]*Snake.BLOCK_SIZE
+        self.y += DIR_OFFSET[self.direction][1]*Snake.BLOCK_SIZE
         self.wait_time = 0
     MOVE_WAIT = 0.2
  
